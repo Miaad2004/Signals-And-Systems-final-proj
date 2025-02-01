71d46494c66711a457c86e991a75fac0b2242735
Miaad <Miaad@MIAAD>
2025-02-02 01:32:02 +0330

    update part 3

--- src/part3/main.m
+++ src/part3/main.m
@@ -1,4 +1,4 @@
-%a
+% الف
 imagePath = 'Resources/Flower.png';
 
 I = imread(imagePath);
@@ -7,15 +7,42 @@ if size(I, 3) == 3
     I = rgb2gray(I);
 end
 
-%b
+fprintf('n_channels= %d\n', size(I, 3));
+
+% ب
 imshow(I);
-title('Grayscale Image of Flower');
+title('Flower');
 
 [rows, cols] = size(I);
-fprintf('Image dimensions: %d x %d\n', rows, cols);
+fprintf('Image dims: %d x %d\n', rows, cols);
 
 dataType = class(I);
-fprintf('Data type of the image: %s\n', dataType);
+fprintf('Image data type: %s\n', dataType);
 
-bitsPerPixel = 8 * whos('I').bytes / (rows * cols);
-fprintf('Each pixel contains %.0f bits.\n', bitsPerPixel);
+bitsPerPixel = 8 * numel(I) / (rows * cols);
+fprintf('Bits per pixel= %.2f\n', bitsPerPixel);
+
+% ج
+quantLevels = [64,33,16,8,4,2];
+nBits = [6,5,4,3,2,1];
+
+figure;
+numQuant = numel(quantLevels);
+
+for idx = 1:numQuant
+    L = quantLevels(idx);  
+    
+    % normalize
+    I_normalized = double(I) / 255;     
+
+    % quantize
+    I_quantized  = round(I_normalized * (L-1)); 
+    
+    % Rescale 
+    Iq = uint8(round(I_quantized * (255/(L-1))));
+    
+    subplot(2, 3, idx);
+    imshow(Iq);
+    title(sprintf('%d Levels (%d-bit)', L, nBits(idx)));
+end
+
