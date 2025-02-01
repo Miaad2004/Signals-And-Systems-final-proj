02ae842513869b255d6d687a87348375df8bc50e
Miaad <Miaad@MIAAD>
2025-02-01 23:21:21 +0330

    Update prj1

--- src/part1/main.m
+++ src/part1/main.m
@@ -1,48 +1,69 @@
-%a
+% الف
 soundData = load('Resources/Sound.mat'); 
 y = soundData.Noisy_Sound; 
 Fs = soundData.Fs;          
-disp(['Sampling rate: ', num2str(Fs), ' Hz']);
+disp(['Fs: ', num2str(Fs), ' Hz']);
 N = length(y);
-disp(['Number of samples: ', num2str(N)]);
+disp(['N_samples: ', num2str(N)]);
 
 %sound(y, Fs);
+%pause(length(y)/Fs);
 
-%b
+% ب
 Y = fft(y); 
 Y_shifted = fftshift(Y); 
-magnitude_spectrum = abs(Y_shifted); 
+magnetude_spec = abs(Y_shifted); 
+
+f_sig = linspace(-Fs/2, Fs/2, N);
 
 figure;
-plot(f, magnitude_spectrum, 'b'); 
+plot(f_sig, magnetude_spec, 'b'); 
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
+title('Magnitude Spectrum');
 grid on;
 
 
-%d_e
+%د
+% completed in fdatool panel
+% Fpass = 6050, Fstop = 6150, Fs=44100
 
-y_filtered = filter(Num, 1, y);
-
-figure;
-subplot(2,1,1);
-plot(y);
-title('Original Noisy Signal');
-xlabel('Samples');
-ylabel('Amplitude');
-grid on;
-
-subplot(2,1,2);
-plot(y_filtered);
-title('Filtered Signal');
-xlabel('Samples');
-ylabel('Amplitude');
-grid on;
-
-sound(y_filtered, Fs);
+%ه
 figure;
 stem(Num, 'filled', 'MarkerFaceColor', 'b');
 xlabel('Sample Index');
 ylabel('Amplitude');
-title('Impulse Response of the Filter');
+title('Impulse Response');
 grid on;
+
+% و
+N_filter = 1024; 
+H = fft(Num, N_filter);  
+H_shifted = fftshift(H); 
+H_magnitude = abs(H_shifted);
+
+f_filter = linspace(-Fs/2, Fs/2, N_filter); 
+
+figure;
+plot(f_filter, H_magnitude, 'b');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('Frequency Response');
+grid on;
+
+% To Dos: Find Cutoff Freq in digital (big OMEGA in oppenhime book)
+% To Dos: Find Cutoff Freq in analog (f in oppenhime book)
+% To DO: omapre the result with design specifications above
+
+
+% ز
+Filtered_Sound = conv(y, Num, 'same');
+sound(Filtered_Sound, Fs);
+
+% ح
+audiowrite("Resources\Filtered_Sound.wav", Filtered_Sound, Fs);
+
+% ط
+
+
+
